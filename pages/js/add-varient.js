c8216cc30e66ce3fb3687a50e297e07f1135e5c3
unknown <pradip1232>
2024-01-03 13:23:02 -0500

    chnage somthing

---
+++ pages/js/add-varient.js
@@ -0,0 +1,10 @@
+
+// $(document).ready(function () {
+//     $("#addRowBtn").click(function () {
+//         var newRow = $("#variantsContainer .row:first").clone();
+//         newRow.find("input").val(""); // Clear input values in the new row
+//         $("#variantsContainer").append(newRow);
+//     });
+// });
+
+ 
